bd16d31e89a6d170d0e9b1213a03a0ddf47d06ab
Csengori <csengori@gmail.com>
2018-06-19 16:16:15 +0100

    Changed root to TabBarController

--- Xcode/WrittenMyRecipes/WrittenMyRecipes/SupportingFiles/AppDelegate.swift
+++ Xcode/WrittenMyRecipes/WrittenMyRecipes/SupportingFiles/AppDelegate.swift
@@ -18,18 +18,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow()
         window?.makeKeyAndVisible()
         
+        //CollectionViewSetup
         let collectionViewLayout = UICollectionViewFlowLayout()
         collectionViewLayout.minimumLineSpacing = 16
         collectionViewLayout.minimumInteritemSpacing = 0
         collectionViewLayout.scrollDirection = .vertical
-       
+        
         let navigationController = UINavigationController(rootViewController: MainViewController(collectionViewLayout: collectionViewLayout))
-       // let mainViewController = MainViewController(collectionViewLayout: collectionViewLayout)
-       
-        window?.rootViewController = navigationController
+
+        
+        let tabBarController  = UITabBarController()
+      
+        //Setup SubViewControllers
+        let viewControllers = [navigationController]
+        tabBarController.viewControllers = viewControllers
+        tabBarController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(named: "pot"), tag: 1)
+      
+        window?.rootViewController = tabBarController
         
         return true
     }
+    
+    
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
